e0684c5fcac4c3f4e23e67eeeb5feac79287162c
BPplays <andymalbp@gmail.com>
2024-04-06 01:13:09 -0700

    bleh

--- laptop/run.sh
+++ laptop/run.sh
@@ -1,8 +1,12 @@
 #! /usr/bin/zsh
 
+set -e
+
+
 setopt nocorrect
 pacman -Sy --noconfirm git
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-git clone git@github.com:BPplays/arch-auto-installs.git
+# git clone git@github.com:BPplays/arch-auto-installs.git
+git clone https://github.com/BPplays/arch-auto-installs.git
 cd ./arch-auto-installs/laptop
 ./aui.sh
